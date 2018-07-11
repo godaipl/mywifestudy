c08243b982c643ea26f0d510819d7fbd74c22830
yandongjun <yandongjun@treefinance.com.cn>
2018-07-11 11:33:31 +0800

    添加基础测试题目1

---
+++ wife-study/src/main/java/com/china/bobo/study/wifestudy/贰java基础学习/java基础测试题目/壹模拟计算器.java
@@ -0,0 +1,15 @@
+package com.china.bobo.study.wifestudy.贰java基础学习.java基础测试题目;
+
+/**
+ * @ClassName 壹模拟计算器
+ * @Description 模拟计算器的实现
+ * @Author yandongjun
+ * @Date 2018/7/11 上午11:31
+ * @Version 1.0
+ **/
+public class 壹模拟计算器 {
+    // 模拟计算器的除法运算
+    // 1. 输入被除数
+    // 2. 输入除数
+    // 3. 计算结果并输出
+}
