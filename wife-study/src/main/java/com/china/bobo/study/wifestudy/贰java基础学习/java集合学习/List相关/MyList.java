e0d5a812d505f7e5a0a47d8023bcaefd50459a40
yandongjun <yandongjun@treefinance.com.cn>
2018-08-08 14:17:41 +0800

    添加集合作业

---
+++ wife-study/src/main/java/com/china/bobo/study/wifestudy/贰java基础学习/java集合学习/List相关/MyList.java
@@ -0,0 +1,36 @@
+package com.china.bobo.study.wifestudy.贰java基础学习.java集合学习.List相关;
+
+/**
+ * @ClassName MyList
+ * @Description TODO
+ * @Author yandongjun
+ * @Date 2018/8/8 下午1:55
+ * @Version 1.0
+ **/
+public class MyList {
+    private Object[] objects = null;
+
+    public MyList() {
+        objects = new Object[10000];
+    }
+
+    public MyList(Integer capacity) {
+        objects = new Object[capacity];
+    }
+
+    public Object get(int index) {
+        return null;
+    }
+
+    public void set(int index, Object o) {
+        
+    }
+
+    public void add(Object o) {
+
+    }
+
+    public boolean del(Integer index) {
+        return false;
+    }
+}
---
+++ wife-study/src/main/java/com/china/bobo/study/wifestudy/贰java基础学习/java集合学习/List相关/MyListTest.java
@@ -0,0 +1,20 @@
+package com.china.bobo.study.wifestudy.贰java基础学习.java集合学习.List相关;
+
+/**
+ * @ClassName MyListTest
+ * @Description TODO
+ * @Author yandongjun
+ * @Date 2018/8/8 下午2:16
+ * @Version 1.0
+ **/
+public class MyListTest {
+    public static void main(String[] args) {
+        // 初始化集合
+
+        // 往集合中添加数据
+
+        // 从集合中删除数据
+
+        // 遍历打印集合中各元素的内容
+    }
+}
